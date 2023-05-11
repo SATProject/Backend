a96dbe4420aa3c42882a459f017f9057c3805c07
cafebazaar <cafebazaar@cafebazaars-MacBook-Pro-6.local>
2023-05-11 15:34:05 +0330

    done

--- api/views.py
+++ api/views.py
@@ -5,17 +5,19 @@ from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
+import json
 
 from core import main
 
-
 class Chat(APIView):
-    state = -1
-    FINAL_STATE = 5
+    state = "GREETING"
+    FINAL_STATE = "END2"
+    suggested_protocol_pool = []
 
     def post(self, request):
-        Chat.state += 1
+        message = json.loads(request.body.decode('utf-8'))["message"]
+        res, Chat.state, Chat.suggested_protocol_pool = main.information_retrieval_module(Chat.state, message, Chat.suggested_protocol_pool)
         if Chat.state != Chat.FINAL_STATE:
-            return Response({"status": "success", "response": main.information_retrieval_module(Chat.state)},
+            return Response({"status": "success", "response": res},
                             status=status.HTTP_200_OK)
-        return Response({"status": "success", "response": "ممنون که ما را همراهی کردید."}, status=status.HTTP_200_OK)
+        return Response({"status": "success", "response":  "امیدوارم تونسته باشم کمکت کنم."}, status=status.HTTP_200_OK)
