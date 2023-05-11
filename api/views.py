8a89be6ec26942eabd170d1b5f078c9fcab0d5c6
cafebazaar <cafebazaar@cafebazaars-MacBook-Pro-6.local>
2023-05-11 18:10:24 +0330

    updates

--- api/views.py
+++ api/views.py
@@ -12,12 +12,13 @@ from core import main
 class Chat(APIView):
     state = "GREETING"
     FINAL_STATE = "END2"
+    buttons = []
     suggested_protocol_pool = []
 
     def post(self, request):
         message = json.loads(request.body.decode('utf-8'))["message"]
-        res, Chat.state, Chat.suggested_protocol_pool = main.information_retrieval_module(Chat.state, message, Chat.suggested_protocol_pool)
+        res, Chat.state, Chat.suggested_protocol_pool, Chat.buttons = main.information_retrieval_module(Chat.state, message, Chat.suggested_protocol_pool)
         if Chat.state != Chat.FINAL_STATE:
-            return Response({"status": "success", "response": res},
+            return Response({"status": "success", "response": res, "buttons": Chat.buttons},
                             status=status.HTTP_200_OK)
-        return Response({"status": "success", "response":  "امیدوارم تونسته باشم کمکت کنم."}, status=status.HTTP_200_OK)
+        return Response({"status": "success", "response":  "امیدوارم تونسته باشم کمکت کنم.", "buttons": Chat.buttons}, status=status.HTTP_200_OK)
