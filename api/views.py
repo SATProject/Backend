13c37f6dea2bfa2a9d8855a369e1b076a3b6d8a9
cafebazaar <cafebazaar@cafebazaars-MacBook-Pro-6.local>
2023-07-28 15:00:07 +0330

    add daily diary

--- api/views.py
+++ api/views.py
@@ -21,12 +21,28 @@ class Chat(APIView):
     dof = 0
     previous_questions_embeddings = []
     model = SentenceTransformer('HooshvareLab/bert-base-parsbert-uncased')
+    protocol = ""
+    last_state = "GREETING"
   
     def post(self, request):
         message = json.loads(request.body.decode('utf-8'))["message"]
         if message == "restart":
             Chat.state = "GREETING"
+
+        ## USAGE: DAILY DIARY
+        Chat.last_state = Chat.state
+        ###
+
         res, Chat.state, Chat.suggested_protocol_pool, Chat.buttons, Chat.additionals, Chat.addtional_num, Chat.name, Chat.dof, Chat.previous_questions_embeddings = main.information_retrieval_module(Chat.state, message, Chat.suggested_protocol_pool, Chat.additionals, Chat.addtional_num, Chat.name, Chat.dof, Chat.previous_questions_embeddings, Chat.model)
+        
+        ## DAILY DIARY
+        if res == "لطفا تمرین زیر رو انجام بده.":
+            Chat.protocol = res.title
+        if Chat.last_state == "PROTOCOL_SUGGESTING3":
+            with open('../core/daily_diary.txt', 'a') as f:
+                f.write('protocol:', Chat.protocol, 'message:', message)
+        ###
+
         if Chat.state != Chat.FINAL_STATE:
             return Response({"status": "success", "response": res, "buttons": Chat.buttons},
                             status=status.HTTP_200_OK)
